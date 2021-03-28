712e8531bb36e9514b56c37860b506f44ede506d
ShashankHollaS <53040532+ShashankHollaS@users.noreply.github.com>
2021-03-28 11:40:34 +0530

    Update getChannel.m

--- getChannel.m
+++ getChannel.m
@@ -4,11 +4,11 @@ sumReceivedSignal = sum(receivedSignal(:, :, 1), 2); % summation Zi
 noise = zeros(500, 1);
 % summation noise
 for i=1:4096
-    noise = noise + (sigma/sqrt(2)) * randn(500, 1);
+    noise = noise + (sqrt(sigma)/sqrt(2)) * randn(500, 1);
 end
-F = DFT(K);
-hd_plus_vi1 = (F\sumReceivedSignal-noise) ./ transmitSignal; % eqn (3)
+%F = DFT(K);
+hd_plus_vi1 = ((sumReceivedSignal-noise) ./ transmitSignal)/4096; % eqn (3)
 
-noise = (sigma/sqrt(2)) * randn(500, 4096);
-tmp = (F\(receivedSignal(:, :, 1) - noise)) ./ transmitSignal;
+noise = (sqrt(sigma)/sqrt(2)) * randn(500, 4096);
+tmp = ((receivedSignal(:, :, 1) - noise)) ./ transmitSignal;
 channel = (tmp - hd_plus_vi1)/(pilotMatrix); % eqn (4)
