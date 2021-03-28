3313af87a9e03a3137f8842e50ec00f1db28eb77
karthik-r-rao <karthikrrao30@gmail.com>
2021-03-28 11:44:51 +0530

    add getChannel

---
+++ karthik/getChannel.m
@@ -0,0 +1,17 @@
+load("dataset2.mat")
+getNoise
+sumReceivedSignal = sum(receivedSignal(:, :, 1), 2); % summation Zi
+noise = zeros(500, 1);
+% summation noise
+for i=1:4096
+    noise = noise + (sqrt(sigma)/sqrt(2)) * randn(500, 1);
+end
+%F = DFT(K);
+hd_plus_vi1 = ((sumReceivedSignal-noise)) ./ transmitSignal; % eqn (3)
+hd_plus_vi1 = hd_plus_vi1/4096;
+
+noise = (sqrt(sigma)/sqrt(2)) * randn(500, 4096);
+tmp = ((receivedSignal(:, :, 1) - noise)) ./ transmitSignal;
+tmp1 = zeros(500, 4096);
+tmp1(:, 1) = hd_plus_vi1;
+channel = (tmp - tmp1)/(pilotMatrix); % eqn (4)
