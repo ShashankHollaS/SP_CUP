8446696b506efad532985168b2a5d8010d04a52c
ShashankHollaS <53040532+ShashankHollaS@users.noreply.github.com>
2021-04-15 15:26:39 +0530

    Update getChannelcorrect.m

--- Shashank/getChannelcorrect.m
+++ Shashank/getChannelcorrect.m
@@ -1,10 +1,11 @@
+function getChannelcorrect(u)
 load("dataset2.mat")
 getNoise
 
 global variance;
 %sigma = 3.0319e-20;
 global mean_val;
-sumReceivedSignal = sum(receivedSignal(:, :, 20), 2); % summation Zi
+sumReceivedSignal = sum(receivedSignal(:, :, u), 2); % summation Zi
 noise = zeros(500, 1);
 
 % summation noise
@@ -19,9 +20,9 @@ hd_plus_vi1 = ((sumReceivedSignal-noise)) ./ transmitSignal; % eqn (3)
 hd_plus_vi1 = hd_plus_vi1/4096;
 
 noise = mean_val + (sqrt(variance)) * randn(500, 4096);
-tmp = ((receivedSignal(:, :, 20) - noise)) ./ transmitSignal;
+tmp = ((receivedSignal(:, :, u) - noise)) ./ transmitSignal;
 %tmp1 = zeros(500, 4096);
 %tmp1(:, 1) = hd_plus_vi1;
 global channel;
 channel = (tmp - hd_plus_vi1)/(pilotMatrix); % eqn (4)
-disp("channel estimated");
+disp("channel estimated" +num2str(u));
