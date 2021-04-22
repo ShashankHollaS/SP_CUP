e97f9eaaec498df51700b1a08a86a1f46fe317a7
karthik-r-rao <karthikrrao30@gmail.com>
2021-04-22 11:03:54 +0530

    random initialization

---
+++ karthik/random_initialization/optimization1.m
@@ -0,0 +1,56 @@
+getNoise
+
+load dataset2.mat
+%load first_indices_28.mat
+%load first_rates_28.mat
+
+for userNo = 24:25
+    filename = "user" + num2str(userNo);
+    global hd_plus_vi1;
+    global channel;
+    [hd_plus_vi1, channel] = findChannel(receivedSignal, transmitSignal, pilotMatrix, userNo);
+
+    lb = ones(4096,1);
+    ub = 2*ones(4096,1);
+    theta_in = rand(4096, 1);
+    for j = 1:4096
+       if theta_in(j) < 0.5
+           theta_in(j) = 1;
+       else
+          theta_in(j) = 2;
+       end
+    end
+
+    opts = optimoptions(@ga,'InitialPopulationMatrix', theta_in','MaxGenerations',100, 'MaxStallGenerations', 100, 'PopulationSize', 200, 'CrossoverFraction', 0.8, 'PlotFcn', @gaplotbestf);
+    [theta_val, fval, exitflag, output, final_val] = ga(@rate, 4096, [], [], [], [], lb, ub, [], 1:4096, opts);
+
+    opts1 = optimoptions(@ga,'InitialPopulationMatrix', final_val, 'MaxGenerations',60, 'MaxStallGenerations', 60, 'PopulationSize', 300, 'PlotFcn', @gaplotbestf);
+    [theta_val1, fval1, exitflag1, output1, final_val1] = ga(@rate, 4096, [], [], [], [], lb, ub, [], 1:4096, opts1);
+
+    opts2 = optimoptions(@ga,'InitialPopulationMatrix', final_val1, 'MaxGenerations',30, 'MaxStallGenerations', 30, 'PopulationSize', 400,'PlotFcn', @gaplotbestf);
+    [theta_val2, fval2, exitflag2, output2, final_val2] = ga(@rate, 4096, [], [], [], [], lb, ub, [], 1:4096, opts2);
+
+    thetas = [theta_val' theta_val1' theta_val2'];
+    fvals = [fval fval1 fval2];
+
+    save(filename + "_thetas.mat", 'thetas');
+    save(filename + "_fvals.mat", 'fvals');
+end
+  
+function y = rate(theta_tmp)
+theta_tmp = MapVariables(theta_tmp);
+global variance;
+N0 = abs(variance);
+P = 1;
+B = power(10,7);
+y = 0;
+K = 500;
+M = 20;
+global hd_plus_vi1;
+global channel;
+
+for i = 1:500
+        h = hd_plus_vi1(i) + channel(i,2:4096)*(theta_tmp(2:4096)');
+        y = y - (B/(K+M-1))*log2(1 + (P*(abs(h)^2)) / (B*N0));
+end
+end
